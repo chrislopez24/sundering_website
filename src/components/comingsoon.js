614ff1a867bdacc6d08e4214c7fbef7da7f08ca2
Chris Lopez <pagarizabal2@gmail.com>
2020-05-30 15:12:49 +0200

    [Teaser] Coming Soon text is now animated
    added tests

--- src/components/comingsoon.js
+++ src/components/comingsoon.js
@@ -3,33 +3,34 @@ import React from 'react';
 import logo from '../assets/logo_high.png'
 import twitter from "../assets/twitter_icon.png"
 import youtube from "../assets/youtube_icon.png"
-
+import { GreatThings} from 'moving-letters'
 
 import LoadingComponent from './loading'
 import Footer from './footer'
 
-const styles = {
+const stylesIcons = {
   height: "30px",
   padding: "0px 20px 0 0px"
 };
+
+const styledDivSocialMedia = {
+  position: "absolute",
+  marginTop: "47vh"
+}
 
 const ComingSoon = () => {
     return (
           <header className="App-header">
             <img src={logo} className="guild-logo" alt="logo" />
-            <h1 class="ml1">
-              <span class="text-wrapper">
-              <span class="letters">Coming Soon...</span>
-              </span>
-            </h1>
-    
+            <React.Fragment>
+              <GreatThings text='Coming Soon...' />
+            </React.Fragment>
             <LoadingComponent />
-            
-            <div className="social-media">  
+            <div className="social-media" style={styledDivSocialMedia}>  
                     <img src={twitter}  className="twitter-icon" alt="twitter-alt"
-                    style={styles}/>
+                    style={stylesIcons}/>
                     <img src={youtube}  className="youtube-icon" alt="youtube-alt"
-                    style={styles}/>
+                    style={stylesIcons}/>
             </div> 
             
           </header>
---
+++ src/components/comingsoon.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import ComingSoon from './comingsoon';
+
+describe('Component Teaser Tests', () =>{
+    afterEach(cleanup)
+    test('Test if the class .App-header is presented and rendered', () => {
+      const { container } = render(<ComingSoon />);
+      const linkElement = container.querySelector('.App-header')
+      expect(linkElement).toBeInTheDocument();
+    });
+})
