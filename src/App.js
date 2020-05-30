a75c619ad608d2c4b6e773ce0bf1bf56b9c68bed
Cristian Lopez Pagarizabal <cristianlopez@paradigmadigital.com>
2020-05-30 03:14:52 +0200

    [Teaser] Social media repositioning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 // import logo from './logo.svg';
 import logo from './assets/logo_high.png'
+import twitter from "./assets/twitter_icon.png"
+import youtube from "./assets/youtube_icon.png"
+
 import './App.css';
 
 import LoadingComponent from './components/loading'
 import Footer from './components/footer'
 
-
+const styles = {
+  height: "30px",
+  padding: "0px 20px 0 0px"
+};
 
 function App() {
   return (
@@ -18,8 +24,14 @@ function App() {
         </p>
 
         <LoadingComponent />
+        
+        <div className="social-media">  
+                <img src={twitter}  className="twitter-icon" alt="twitter-alt"
+                style={styles}/>
+                <img src={youtube}  className="youtube-icon" alt="youtube-alt"
+                style={styles}/>
+        </div> 
       </header>
-      <Footer />
     </div>
   );
 }
